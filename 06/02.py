efd55240d835e71e4d8ed11e9c6541d0081e7df8
Garrit Schaap <github@pixelkind.com>
2022-12-06 10:29:07 +0100

    Day 6

---
+++ 06/02.py
@@ -0,0 +1,16 @@
+file = open('./input.txt', 'r')
+string = file.read()
+
+total_count = 0
+chars = []
+for char in string:
+  if char in chars:
+    index = chars.index(char) + 1
+    chars = chars[index:]
+  
+  chars.append(char)
+  total_count += 1
+  if len(chars) == 14:
+    break
+
+print(total_count)
